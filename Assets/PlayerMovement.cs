251b6230f57b20651003bcbf9403825fd8fc2dbb
spquyt <spquyt@gmail.com>
2020-04-01 18:15:16 +0700

    Add Slippery and Control button for object - Lesson 3

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //rb.AddForce(0, 200, 500);
-    }
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -1500 * Time.deltaTime);
+        }
     }
 }
